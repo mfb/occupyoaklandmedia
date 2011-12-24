7b92f72a6e2eefb4a025ae51d7834fcff992ebb4
mark burdett <mfburdett@gmail.com>
2011-12-24 21:53:43 +0000

    try downloading both flv and mp4

--- livestream/downloader.php
+++ livestream/downloader.php
@@ -14,8 +14,9 @@ foreach ($library->channel->children($namespaces['ls'])->directory as $directory
 foreach ($items as $item) {
   preg_match('!^http://www.livestream.com/filestore/user-files/choccupyoakland/([0-9]{4}/[0-9]{2}/[0-9]{2}/[a-z0-9-]+)_[0-9]_low.jpg$!', $item->thumbnailLow, $matches);
   if (isset($matches[1])) {
-    $url = 'http://clipdownload.livestream.com/mogulus-user-files/choccupyoakland/' . $matches[1] . '.mp4';
-    passthru('wget -c ' . escapeshellarg($url));
+    $url = 'http://clipdownload.livestream.com/mogulus-user-files/choccupyoakland/' . $matches[1];
+    passthru('wget -c ' . escapeshellarg($url . '.mp4'));
+    passthru('wget -c ' . escapeshellarg($url . '.flv'));
   }
   else {
     trigger_error("MISSING THUMBNAIL - CANNOT GENERATE DOWNLOAD URL");
